cae6c22f83f6519572bf3b5e0fc7acc14c99efc5
ska <shvilpo@gmail.com>
2021-09-14 18:02:33 +0300

    Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal sealed class SomeClass
+    {
+        int a = 1;
+
+        public SomeClass()
+        {
+            a = 2;
+        }
+
+        public override string ToString()
+        {
+            return a.ToString();
+        }
+    }
+    internal struct Point
+    {
+        public Int32 m_x, m_y;
+        public Point(Int32 x, Int32 y)
+        {
+            m_x = x;
+            m_y = y;
+        }
+    }
+    internal sealed class Rectangle
+    {
+        public Point m_topLeft, m_bottomRight;
+        public Rectangle()
+        {
+            // В C# оператор new, использованный для создания экземпляра значимого
+            // типа, вызывает конструктор для инициализации полей значимого типа
+            m_topLeft = new Point(1, 2);
+            m_bottomRight = new Point(100, 200);
+        }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass cl = new SomeClass();
+            Console.WriteLine(cl.ToString());
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+public sealed class Program
+{
+    public Int32 GetFive() { return 5; }
+    public static void Main()
+    {
+        Program p = null;
+        Int32 x = p.GetFive(); // В C# выдается NullReferenceException
+    }
+}
