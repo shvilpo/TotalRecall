88aa45001a43645a2db1603e9b593078a82cda23
Konstantin A. Shvilpo <shvilpo@gmail.com>
2021-09-14 22:33:40 +0300

    Add TypeConstructor

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -23,10 +23,28 @@ namespace Methods
     internal struct Point
     {
         public Int32 m_x, m_y;
+
+        //public Point()
+        //{
+        //    m_x = m_y = 5;
+        //}
         public Point(Int32 x, Int32 y)
         {
             m_x = x;
             m_y = y;
+        }
+        public Point(Int32 x)
+        {
+            // экземпляру значимого типа this можно приписать значение нового экземпляра хначимого типа
+            // в конструкторах ссылочного типа указатель this - только для чтения и присваивать значения ему нельзя
+            this = new Point();
+            m_x = x;
+            m_y = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"({m_x}, {m_y})";
         }
     }
     internal sealed class Rectangle
@@ -39,12 +57,21 @@ namespace Methods
             m_topLeft = new Point(1, 2);
             m_bottomRight = new Point(100, 200);
         }
-        class Program
+        public override string ToString()
+        {
+            return $"m_topLeft: {m_topLeft.ToString()} - bottomright: {m_bottomRight.ToString()}";
+        }
+    }
+    class Program
     {
         static void Main(string[] args)
         {
-            SomeClass cl = new SomeClass();
-            Console.WriteLine(cl.ToString());
+            Rectangle rec = new();
+            Console.WriteLine(rec.ToString());
+            Point poi = new Point();
+            Console.WriteLine(poi);
+
         }
     }
 }
+
---
+++ Methods/TypeConstructor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal sealed class SomeRefType
+    {
+        static SomeRefType()
+        {
+        }
+    }
+
+    internal struct SomeValType
+    {
+        static SomeValType()
+        {
+        }
+    }
+}
